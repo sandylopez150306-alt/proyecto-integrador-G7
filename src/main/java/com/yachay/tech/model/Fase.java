a2d474523d0655411fe42a02483b4e90b42fc84a
Gabriel <i202332157@cibertec.edu.pe>
2026-04-02 18:59:57 -0500

    feat: mapeo de la base de datos

---
+++ src/main/java/com/yachay/tech/model/Fase.java
@@ -0,0 +1,26 @@
+package com.yachay.tech.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "fases")
+public class Fase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_fase")
+    private int idFase;
+
+    @Column(name = "numero_fase", nullable = false, unique = true)
+    private Integer numeroFase;
+
+    @Column(name = "nom_fase", nullable = false, length = 100)
+    private String nomFase;
+
+    @Column(name = "desc_Fase", columnDefinition = "TEXT")
+    private String descFase;
+}
